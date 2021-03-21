4415e75bcb5a18319b481743a6a2a1aac6f340de
vandesm14 <vandesm14@gmail.com>
2021-03-21 15:19:24 -0400

    basic dropdown

---
+++ docs/commands.js
@@ -0,0 +1,56 @@
+const clui = (()=>{
+	const clui = {
+		commands: {
+			'test': {
+				description: 'tests a specific subsystem',
+				commands: {
+					'cli': {
+						description: 'tests the cli parser and components',
+						args: [
+							{
+								name: 'string',
+								description: 'either a single word or a multi-word string using quotes',
+								type: 'string',
+								required: true
+							},
+							{
+								name: 'number',
+								description: 'either an integer or a float',
+								type: ['integer', 'float'],
+								required: true
+							},
+							{
+								name: 'optional',
+								description: 'a boolean value: true or false',
+								type: 'boolean',
+								// required is inferred as false
+								isArg: true
+								// argument is forced to be an arg, not a flag
+							},
+							{
+								name: ['flag', 'f'],
+								description: 'a small boolean flag',
+								type: 'boolean'
+								// required is inferred as false
+								// argument is inferred as a flag (--flag or -f)
+							},
+							{
+								name: 'flag-string',
+								description: 'a flag with a single argument',
+								type: 'string'
+								// required is inferred as false
+								// argument is inferred as a flag (--flag or -f)
+							}
+						],
+						run: (args, state, gui) => {
+							console.log(args, state, gui);
+							// gui.render(args); // renders the args onto the GUI page
+						}
+					}
+				}
+			}
+		}
+	};
+
+	return clui;
+})();
--- src/Index.svelte
+++ src/Index.svelte
@@ -1,9 +1,65 @@
 <script>
 </script>
 
-<main>
-	Hi
-</main>
+<div class="clui-cli">
+	<div class="clui-cli-input">
+		<img src="" alt="" class="clui-cli-icon">
+		<div class="clui-cli-autocomplete"></div>
+		<input type="text" placeholder="enter a command">
+	</div>
+	<div class="clui-cli-dropdown">
+		{#each Object.keys(clui.commands) as command}
+			<div class="clui-dropdown-item">
+				<span class="clui-dropdown-name">{command}</span>
+				<span class="clui-dropdown-description">{clui.commands[command].description}</span>
+			</div>
+		{/each}
+	</div>
+</div>
 
 <style>
+	.clui-cli {
+		display: flex;
+		flex-direction: column;
+		width: max-content;
+		margin: 4vh auto;
+	}
+
+	.clui-cli-input {
+		display: flex;
+		align-items: middle;
+		background-color: var(--medium);
+	}
+
+	.clui-cli-input > input {
+		background-color: var(--medium);
+		color: white;
+		border: none;
+		outline: none;
+		font-size: 1.2rem;
+		padding: 0.5rem 1rem;
+		font-family: Calibri, sans-serif;
+		width: 30vw;
+	}
+
+	.clui-cli-dropdown {
+		background-color: var(--medium);
+		color: white;
+		font-size: 1.2rem;
+	}
+
+	.clui-dropdown-item {
+		padding: 0.6rem 1.2rem;
+	}
+	.clui-dropdown-item:hover, .clui-dropdown-item.clui-selected {
+		background-color: var(--light);
+		cursor: pointer;
+	}
+
+	.clui-dropdown-name {
+		background-color: var(--light);
+		padding: 0.3rem 0.5rem;
+		margin-left: -0.6rem;
+		margin-right: 0.3rem;
+	}
 </style>
