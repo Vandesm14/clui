fa3049d05c41eb7a143148ec2577ee2b18610fe2
Shane Vandegrift <vandesm14@gmail.com>
2021-07-10 10:56:43 -0400

    add semicolons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import App from './App.svelte'
+import App from './App.svelte';
 
 const app = new App({
 	// @ts-expect-error
   target: document.getElementById('clui')
-})
+});
 
-export default app
+export default app;
