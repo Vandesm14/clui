554460ee3eb67568886effe6383d6d30e7640748
Shane Vandegrift <vandesm14@gmail.com>
2021-07-19 22:51:40 -0400

    move Command and Arg to classes

--- src/clui.types.ts
+++ src/clui.types.ts
@@ -1,35 +1,64 @@
-export type Command = CommandEmpty | CommandArgs | CommandCmd;
+// export type Command = CommandEmpty | CommandArgs | CommandCmd;
 
-interface CommandBase {
+// interface CommandBase {
+// 	name: string,
+// 	description?: string,
+// 	run?: (ctx: RunCtx, args: Arg[]) => void
+// }
+
+// interface CommandEmpty extends CommandBase {
+// 	type?: undefined,
+// 	children?: undefined
+// }
+
+// interface CommandArgs extends CommandBase {
+// 	type: 'arg',
+// 	children?: Arg[];
+// }
+
+// interface CommandCmd extends CommandBase {
+// 	type: 'cmd',
+// 	children?: Command[];
+// }
+
+export interface Command {
 	name: string,
 	description?: string,
+	type?: 'cmd' | 'arg',
+	children?: Command[] | Arg[],
 	run?: (ctx: RunCtx, args: Arg[]) => void
 }
 
-interface CommandEmpty extends CommandBase {
-	type?: undefined,
-	children?: undefined
-}
-
-interface CommandArgs extends CommandBase {
-	type: 'arg',
-	children?: Arg[];
-}
-
-interface CommandCmd extends CommandBase {
-	type: 'cmd',
-	children?: Command[];
+export class Command {
+	constructor(obj: Command) {
+		this.name = obj.name;
+		this.description = obj.description;
+		this.type = obj.type;
+		this.children = obj.children;
+		this.run = obj.run;
+	}
 }
 
 export interface Arg {
 	name: string,
+	description?: string,
 	type: 'string' | 'number' | 'boolean',
 
-	/** arg: a parameter (e.g. `ls __~/Desktop__`), opt: an option (e.g. `-a` or `--flag param`) */
+	/** arg: a parameter (e.g. ls __~/Desktop__), opt: an option (e.g. __-a__ or __--flag param__) */
 	mode?: 'arg' | 'opt',
 	required?: boolean,
-	description?: string,
 	default?: any
+}
+
+export class Arg {
+	constructor(obj: Arg) {
+		this.name = obj.name;
+		this.description = obj.description;
+		this.type = obj.type;
+		this.mode = obj.mode;
+		this.required = obj.required;
+		this.default = obj.default;
+	}
 }
 
 export interface RunCtx {
