db8abf23fb9b33160c8584cfe4ede0a6636bdbcb
Shane Vandegrift <vandesm14@gmail.com>
2021-07-23 00:06:00 -0400

    reformat clui tests

--- src/tests/clui.test.ts
+++ src/tests/clui.test.ts
@@ -47,55 +47,61 @@ describe('clui', () => {
 	});
 
 	describe('parser & matcher (parseMatch)', () => {
+		beforeEach(() => {
+			clui = new CLUI(); // reset the CLUI instance
+			clui.load(git);
+		});
 		it('match a command', () => {
-			clui.load(...many);
-
-			many.forEach(command => {
-				expect(clui.parseMatch(command.name)).toEqual([command]);
-			});
+			expect(clui.parseMatch('git')).toEqual([git]);
 		});
 		it('match a sub-command', () => {
-			clui.load(git);
-
 			expect(clui.parseMatch('git push')).toEqual([git, push]);
 		});
 		it('match all tokens (cmd | arg)', () => {
-			clui.load(git);
-
-			expect(clui.parseMatch("git push origin"))
+			expect(clui.parseMatch('git push origin'))
       .toEqual([
         git,
         push,
-        { ...push.children[0], value: "origin" },
+        { ...push.children[0], value: 'origin' },
       ]);
-      expect(clui.parseMatch("git push origin master"))
+      expect(clui.parseMatch('git push origin master'))
       .toEqual([
         git,
         push,
-        { ...push.children[0], value: "origin" },
-        { ...push.children[1], value: "master" },
+        { ...push.children[0], value: 'origin' },
+        { ...push.children[1], value: 'master' },
       ]);
-      expect(clui.parseMatch("git push origin master -f"))
+      expect(clui.parseMatch('git push origin master -f'))
       .toEqual([
         git,
         push,
-        { ...push.children[0], value: "origin" },
-        { ...push.children[1], value: "master" },
+        { ...push.children[0], value: 'origin' },
+        { ...push.children[1], value: 'master' },
         { ...push.children[2], value: true },
       ]);
 		});
     it('match & return unknown tokens', () => {
-			clui.load(git);
-
       expect(clui.parseMatch('git hello origin master -f'))
       .toEqual([
         git,
         { type: 'cmd', val: 'hello', unknown: true },
-        { type: 'cmd', val: "origin", unknown: true },
-        { type: 'cmd', val: "master", unknown: true },
+        { type: 'cmd', val: 'origin', unknown: true },
+        { type: 'cmd', val: 'master', unknown: true },
         { type: 'opt', val: 'f', unknown: true },
       ]);
     });
+		it('match tokens with custom root element', () => {
+			expect(clui.parseMatch('push origin', new Command(git)))
+      .toEqual([
+        push,
+        { ...push.children[0], value: 'origin' },
+      ]);
+      expect(clui.parseMatch('origin master', new Command(push)))
+      .toEqual([
+        { ...push.children[0], value: 'origin' },
+        { ...push.children[1], value: 'master' },
+      ]);
+		});
 		it('invalid root type', () => {
 			// @ts-expect-error
       expect(() => clui.match({}, clui.parse(''))).toThrow('Invalid root type (root has to be a Command)');
