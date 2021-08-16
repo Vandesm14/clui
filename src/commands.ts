8b974729e7370d3a5acc24e70e12145365edd933
Shane Vandegrift <vandesm14@gmail.com>
2021-08-16 19:44:46 -0400

    add 2 help commands

--- src/commands.ts
+++ src/commands.ts
@@ -1,52 +1,57 @@
-import test from './tests/clui_many_commands';
 import type { Command, Arg } from './clui';
 
-const commands: Command[] = [
-	{
-		name: 'clui',
-		description: 'the main system (so meta)',
-		type: 'cmd',
-		children: [
-			{
-				name: 'help',
-				description: 'get help with the CLUI',
-				type: 'arg',
-				children: [
-					{
-						name: 'command',
-						description: 'the command to get help with',
-						type: 'string'
-					}
-				],
-				run: (ctx, args: Arg[]) => {
-					const command = args[0];
-					if (command) {
-
-					} else {
-					}
+const clui: Command = 	{
+	name: 'clui',
+	description: 'the main system (so meta)',
+	type: 'cmd',
+	children: [
+		{
+			name: 'help',
+			description: 'get help with the CLUI',
+			type: 'arg',
+			children: [
+				{
+					name: 'command',
+					description: 'the command to get help with',
+					type: 'string'
 				}
-			},
-			{
-				name: 'config',
-				description: 'configure the CLUI',
-				type: 'arg',
-				children: [
-					{
-						name: 'option',
-						description: 'the option to configure',
-						type: 'string',
-						required: true
-					},
-					{
-						name: 'value',
-						description: 'the value to set',
-						type: 'string',
-						required: true
-					}
-				]
+			],
+			run: (ctx, args: Arg[]) => {
+				const command = args[0];
+				if (command) {
+					// show help for the command
+				} else {
+					// show help for the system
+				}
 			}
-		]
-	}
+		},
+		{
+			name: 'config',
+			description: 'configure the CLUI',
+			type: 'arg',
+			children: [
+				{
+					name: 'option',
+					description: 'the option to configure',
+					type: 'string',
+					required: true
+				},
+				{
+					name: 'value',
+					description: 'the value to set',
+					type: 'string',
+					required: true
+				}
+			]
+		}
+	]
+};
+
+const help = (clui.children as Command[]).find((el: Command) => el.name === 'help');
+
+const commands: Command[] = [
+	clui,
+	help,
 ]
 
 export default commands;
